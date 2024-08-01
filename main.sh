9b8bc7207e055351be9407e946771c03d3f98003
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-08-01 16:18:23 +0300

    port to os 4

--- main.sh
+++ main.sh
@@ -1,7 +1,17 @@
+#! /bin/bash
+
+set -e
+
+VERSION="1.0.1"
+
+source ./pika-build-config.sh
+
+echo "$PIKA_BUILD_ARCH" > pika-build-arch
+
 # Clone Upstream
-mkdir -p ./pika-first-setup
-rsync -av --progress ./* ./pika-first-setup --exclude ./pika-first-setup
-cd ./pika-first-setup
+mkdir -p pika-first-setup
+cp -rvf ./* ./pika-first-setup/ || true
+cd ./pika-first-setup/
 
 # Get build deps
 apt-get build-dep ./ -y
@@ -9,6 +19,7 @@ apt-get install curl -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | CARGO_HOME=/root/.cargo sh -s -- -y
 
 # Build package
+LOGNAME=root dh_make --createorig -y -l -p pika-first-setup_"$VERSION" || echo "dh-make: Ignoring Last Error"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
