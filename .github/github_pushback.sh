86fd11cadb235c0375c83afc905e4d8f93c8dc4b
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-12-04 20:04:56 +0300

    idk

--- .github/github_pushback.sh
+++ .github/github_pushback.sh
@@ -7,12 +7,13 @@ else
     echo "No push back here" && exit 0
 fi
 
-git remote set-url origin git@git.pika-os.com:custom-gui-packages/pika-first-setup
+
 
 # Commit changes to git
 git config --global user.name 'Github Mirror Push Back - Ward'
 git config --global user.email 'hotrod.master@hotmail.com'
 git config --global --add safe.directory /__w/gitea-pika-first-setup/gitea-pika-first-setup
+git remote set-url origin git@git.pika-os.com:custom-gui-packages/pika-first-setup
 git add .
 git commit -am"Github Mirror Push Back"
 git push
