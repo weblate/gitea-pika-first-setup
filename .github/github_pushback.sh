5da2886aa9f5f4b923812c4c0a91bce16e7abba6
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-12-05 16:57:04 +0300

    get rid of dangerous behivour hopefully

--- .github/github_pushback.sh
+++ .github/github_pushback.sh
@@ -16,10 +16,16 @@ ssh-keyscan  -H git.pika-os.com >> ~/.ssh/known_hosts
 # Commit changes to git
 git config --global user.name 'Github Gitea Push Back Key - Cosmo'
 git config --global user.email 'cosmo@pika-os.com'
-git config --global --add safe.directory /__w/gitea-pika-first-setup/gitea-pika-first-setup
-git remote set-url origin git@git.pika-os.com:custom-gui-packages/pika-first-setup
+#git config --global --add safe.directory /__w/gitea-pika-first-setup/gitea-pika-first-setup
+
+git clone git@git.pika-os.com:custom-gui-packages/pika-first-setup
+
+rm -rfv ./gitea-pika-first-setup/.git
+cp ./pika-first-setup/.git ./gitea-pika-first-setup/
+
+cd ./gitea-pika-first-setup
 git add .
 git commit -am"Github Mirror Push Back"
-git config pull.rebase true
-git pull
+#git config pull.rebase true
+#git pull
 git push
